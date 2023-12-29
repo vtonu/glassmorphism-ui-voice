4ea5cda381df7e4683a9ec9d8574a136a3570390
vtonu <victor.tonu@yahoo.com>
2023-12-29 12:11:13 -0800

    Cleaned up GlassUI.jsx and index.css, added squad sounds text and 6 buttons.

--- src/GlassUI.jsx
+++ src/GlassUI.jsx
@@ -1,59 +1,31 @@
-import { useState } from 'react';
-
 function GlassUI() {
-  const [inputValue, setInputValue] = useState('');
-  const [balance, setBalance] = useState(225.55);
-
-  const deposit = () => {
-    setBalance(balance + Number(inputValue));
-    setInputValue('');
-  };
-
-  const withdraw = () => {
-    if (balance >= Number(inputValue)) {
-      setBalance(balance - Number(inputValue));
-      setInputValue('');
-    } else {
-      alert('Insufficient balance');
-    }
-  };
-
-  const clearInput = () => {
-    setInputValue('');
-  };
-
   return (
     <div>
       <div className="upper-layer">
         <img src={require('./close.png')} width="16" alt="saeslogo" className="closeButton" />
         <div className="atmHeader">
           <img src={require('./saeslogo_voice.png')} width="110" alt="saeslogo" />
-          {/* <h2>ATM</h2> */}
-          {/* <h5>Manage your account balance.</h5> */}
         </div>
         <div className="accountBalance">
-          <h6>ACTIVE</h6>
+          {/* <h6>ACTIVE</h6> */}
           {/* <input
-            type="number"
+            type="text"
             value={inputValue}
             onChange={(e) => {
               if (e.target.value.length <= 7) {
                 setInputValue(e.target.value);
               }
             }}
-            max="1000000"
           /> */}
         </div>
+        <h5>SQUAD SOUNDS</h5>
         <div className="buttonsHeader">
-          <button className="button" onClick={clearInput}>
-            VOICE 1
-          </button>
-          <button className="button" onClick={deposit}>
-            VOICE 2
-          </button>
-          <button className="button" onClick={withdraw}>
-            VOICE 3
-          </button>
+          <button className="button">VOICE 1</button>
+          <button className="button">VOICE 2</button>
+          <button className="button">VOICE 3</button>
+          <button className="button">VOICE 4</button>
+          <button className="button">VOICE 5</button>
+          <button className="button">VOICE 6</button>
         </div>
       </div>
     </div>
